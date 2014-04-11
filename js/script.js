255d0b171eae7c999f03920b16481b9971ac4022
Juan Pablo <juampi92@gmail.com>
2014-04-10 21:19:12 -0300

    Histograma & Lenna

--- js/script.js
+++ js/script.js
@@ -44,6 +44,7 @@
 		this.src = src;
 		this.imgData = {};
 		this.width = this.height = 0;
+		this.histograma = null;
 		this.onEvents = {};
 	};
 
@@ -125,10 +126,19 @@
 		if ( onload ) onload(this);
 	};
 
+	Imagen.prototype.loop = function(callback){
+		var i = 0;
+		for ( var y = 0 ; y < this.height ; y++ )
+    		for ( var x = 0 ; x < this.width ; x++ ){
+    			callback(i,x,y,this.imgData.data[i],this.imgData.data[i+1],this.imgData.data[i+2]);
+			    i += 4;
+    		}
+	};
+
 	Imagen.prototype.getBase64 = function(){
 		var canvas = document.createElement('canvas');
-		canvas.width = this.img.width;
-		canvas.height = this.img.height;
+		canvas.width = this.width;
+		canvas.height = this.height;
 
 		var context = canvas.getContext('2d');
 
@@ -217,17 +227,14 @@
 			pixels_dest = this.dest.imgData.data,
     		numPixels = this.source.width * this.source.height;
 
-    	var i = 0;
-    	for ( var y = 0 ; y < this.source.height ; y++ )
-    		for ( var x = 0 ; x < this.source.width ; x++ ){
-    			var rgb = callback(pixels_source[i*4] , pixels_source[i*4+1] , pixels_source[i*4+2] ,x,y);
-    			if ( !rgb ) rgb = {r:pixels_source[i*4],g:pixels_source[i*4+1],b:pixels_source[i*4+2]};
-			    pixels_dest[i*4] = rgb.r;
-			    pixels_dest[i*4+1] = rgb.g; // Green
-			    pixels_dest[i*4+2] = rgb.b; // Blue
-			    i++;
-    		}
-
+    	this.source.loop(function(i,x,y,r,g,b){
+    		var rgb = callback(r,g,b,x,y);
+			if ( !rgb ) rgb = {r:r,g:g,b:b};
+		    pixels_dest[i] = rgb.r;
+		    pixels_dest[i+1] = rgb.g; // Green
+		    pixels_dest[i+2] = rgb.b; // Blue
+    	});
+    	
 		if ( this.events["end"] ) this.events["end"]();
 		if ( onEnd ) onEnd();
 		return;
@@ -243,8 +250,9 @@
 	}
 
 	PDI.prototype.require = function( effectStr , params ){
-		efectos[effectStr].exec(this , params);
+		var ret = efectos[effectStr].exec(this , params);
 		this.snapshot(efectos[effectStr].nom);
+		return ret;
 	};
 
 	PDI.prototype.run = function( effectStr , params ) {
@@ -261,7 +269,7 @@
 	//  	Transformaciones
 	// ************************
 
-	function Transformacion( constructor ){
+	function Transformacion_color( constructor ){
 		this.func = null;
 		if ( $.isNumeric(constructor) ) {
 			var cte = (255 / Math.pow(255,constructor));
@@ -272,12 +280,130 @@
 			this.func = constructor;
 	};
 
-	Transformacion.prototype.exec = function( r, g, b){
+	Transformacion_color.prototype.exec = function( r, g, b){
 		if ( $.isArray( this.func ) )
 			return { r: this.func.r(r) , g:this.func.g(g) , b:this.func.b(b) };
 		else
 			return { r: this.func(r) , g:this.func(g) , b:this.func(b) };
 	}
+
+	// ************************
+	//  	Histograma
+	// ************************
+
+	function Histograma( imagen ){
+		this.img = imagen;
+		this.color = false;
+		this.histograma = null;
+		this.histogramax = 0;
+		this.lut = null; // Look up table
+	};
+
+	Histograma.prototype.calcular = function( color ){
+		var self = this;
+		if ( color ) this.color = true;
+
+		if ( color ) {
+			// Color
+			self.histograma = {r:new Array(256),g:new Array(256),b:new Array(256)};
+			for (var i = 0; i < self.histograma.r.length; i++)
+				self.histograma.r[i] = self.histograma.g[i] = self.histograma.b[i] = 0;
+
+			self.img.loop(function(i,x,y,r,g,b){
+				self.histograma.r[r]++;
+				self.histograma.g[g]++;
+				self.histograma.b[b]++;
+				self.histogramax = Math.max(self.histogramax,self.histograma.r[r],self.histograma.g[g],self.histograma.b[b]);
+			});
+
+		} else {
+			// Blanco y Negro
+			self.histograma = new Array(256);
+			for (var i = 0; i < self.histograma.length; i++) self.histograma[i] = 0;
+
+			this.img.loop(function(i,x,y,r,g,b){
+				self.histograma[r]++;
+				self.histogramax = Math.max(self.histogramax,self.histograma[r]);
+			});
+		}
+		this.histograma = self.histograma;
+	};
+
+	Histograma.prototype.get = function(){
+		return this.histograma;
+	};
+
+	Histograma.prototype.normalizar = function(){
+		if ( this.histograma == null ) this.calcular();
+
+		if ( this.color ) {	
+			this.lut = {r:new Array(256),g:new Array(256),b:new Array(256)};
+			this._normalizar_simple(this.histograma.r , this.lut.r);
+			this._normalizar_simple(this.histograma.g , this.lut.g);
+			this._normalizar_simple(this.histograma.b , this.lut.b);
+		} else {
+			this.lut = new Array(256);
+			this._normalizar_simple(this.histograma , this.lut );
+		}
+	};
+
+	Histograma.prototype._normalizar_simple = function( histograma , lut ){
+		var sum = 0,
+			pixelCount = this.img.width * this.img.height;
+
+		for ( var i = 0; i < 256; ++i ){
+		    sum += histogram[i];
+		    lut[i] = sum * 255 / pixelCount;
+		}
+	};
+
+	Histograma.prototype.getLUT = function( r , g , b  ){
+		if ( this.color )
+			return {
+				r:this.lut.r[r],
+				g:this.lut.g[g],
+				b:this.lut.b[b],
+			};
+		else
+			return this.lut[r];
+	};
+
+	Histograma.prototype.draw = function(){
+		if ( this.histograma == null ) this.calcular();
+
+		var canvas = document.createElement('canvas');
+		canvas.width = ( this.color ) ? 256 * 3 : 256;
+		canvas.height = 400;
+
+		var context = canvas.getContext('2d');
+
+		var factor = ( canvas.height / this.histogramax );
+		if ( this.color ){
+			this._draw(this.histograma.r,'red', context , 0,canvas.height , factor );
+			this._draw(this.histograma.g,'green', context , 256,canvas.height , factor );
+			this._draw(this.histograma.b,'blue', context , 256*2,canvas.height , factor );
+		} else
+		this._draw(this.histograma,'black', context , 0,canvas.height , factor );
+
+		var imagen = new Imagen("");
+		imagen.width = canvas.width;
+		imagen.height = canvas.height;
+		imagen.imgData = context.getImageData(0, 0, canvas.width,canvas.height);
+		return imagen;
+	};
+
+	Histograma.prototype._draw = function( histograma , color , context , offsetX , maxY , factorY){
+		context.beginPath();
+		for ( var i = 0 ; i < 255 ; i++) {
+			x = offsetX+i;
+			context.moveTo(x,maxY);
+			var h = histograma[i] * factorY;
+      		context.lineTo(x,maxY-h);
+		};
+		context.lineWidth = 1;
+		context.strokeStyle = color;
+      	context.stroke();
+	};
 
 	// ************************
 	//  	Efectos
@@ -288,7 +414,7 @@
 			nom: "Negativo",
 			require: null,
 			exec: function(pdi){
-				var trans = new Transformacion(function(c){ return 255-c;});
+				var trans = new Transformacion_color(function(c){ return 255-c;});
 				pdi.loop(function(r,g,b){
 					return trans.exec(r,g,b);
 				});
@@ -306,52 +432,58 @@
 				return;
 			}
 		},
-		"histograma_ByN":  {
-			nom: "Histograma Blanco y Negro",
-			require: null,
-			exec: function(pdi){
-				pdi.require("blanco_negro");
+		"histograma":  {
+			nom: "Histograma",
+			require: [
+				$('<span></span>').html(" Blanco y Negro").prepend(
+					$('<input>').attr("name","byn").attr("type","checkbox"))
+			],
+			exec: function(pdi,params){
+				var color = true;
+				if ( params[0] && params[0].value == "on" ) {
+					color = false;
+					pdi.require("blanco_negro");
+					pdi.dispose();
+				}
 
-				pdi.dispose();
+				pdi.source.histograma = new Histograma(pdi.source);
+				pdi.source.histograma.calcular(color);
 
-				var histograma = new Array(256);
-				for (var i = 0; i < histograma.length; i++)
-					histograma[i] = 0;
+				pdi.dest = pdi.source.histograma.draw();
 
-				pdi.loop(function(b){
-					histograma[b]++;
-				});
-				console.log(histograma);
-				return histograma;
+				return pdi.source.histograma;
 			}
 		},
-		"histograma_color":  {
-			nom: "Histograma a Color",
-			require: null,
-			exec: function(pdi){
-				var histograma = {r:new Array(256),g:new Array(256),b:new Array(256)};
-				for (var i = 0; i < histograma.r.length; i++)
-					histograma.r[i] = histograma.g[i] = histograma.b[i] = 0;
+		"histograma_normal":  {
+			nom: "Normalizacion Histograma",
+			require: [
+				$('<span></span>').html(" Blanco y Negro").prepend(
+					$('<input>').attr("name","byn").attr("type","checkbox"))
+			],
+			exec: function(pdi,params){
+				var hist = "histograma_color",
+					color = true;
+				if ( params[0] && params[0].value == "on" ) { hist = "histograma_ByN"; color = false;}
+
+				var histograma = pdi.require(hist);
+				//pdi.dispose();
 
 				pdi.loop(function(r,g,b){
 					histograma.r[r]++;
 					histograma.g[g]++;
 					histograma.b[b]++;
 				});
-				console.log(histograma);
 				return histograma;
 			}
 		},
-		"transformacion":  {
-			nom: "Transformacion",
+		"transformacion_color":  {
+			nom: "Transformacion de Color",
 			require: [ 
 				$('<select></select>').attr("name","pre-trans")
 					.append( $('<option></option>').attr("value","fact").html("Usar factor ( > 0 )") )
-					.append( $('<option></option>').attr("value","umbral").html("Umbralado (0-255)") ) 
-					//.append( $('<option></option>').attr("value","2").html("2") )
+					.append( $('<option></option>').attr("value","umbral").html("Umbralado (0-255)") )
 					,
 				$('<input>').attr("name","factor").attr("placeholder","Factor")
-					.attr("title","s = r ^ factor")
 					.attr("value","1.5"),
 				$('<span></span>').html(" Blanco y Negro").prepend(
 					$('<input>').attr("name","byn").attr("type","checkbox"))
@@ -361,9 +493,8 @@
 				if ( params ) {
 					if ( params[0].value == "fact" && $.isNumeric(params[1].value) ) factor = params[1].value;
 					else if ( params[0].value == "umbral" && $.isNumeric(params[1].value) ) factor = function(c){
-						return ( c > params[1].value ) ? 0 : 255;
+						return ( c > params[1].value ) ? 255 : 0;
 					};
-					else if ( params[0].value == "someOther" ) factor = 1;
 
 					if ( params[2] && params[2].value == "on" ) {
 						pdi.require("blanco_negro");
@@ -372,14 +503,49 @@
 					}
 				}
 
-				if ( !trans ) trans = new Transformacion(factor);
+				if ( !trans ) trans = new Transformacion_color(factor);
 
-				console.log("Transformando");
 				pdi.loop(function(r,g,b){
 					return trans.exec(r,g,b);
 				});
 			}
 		},
+		/*"transformacion_punto":  {
+			nom: "Transformacion de Punto",
+			require: [ 
+				$('<select></select>').attr("name","pre-trans")
+					.append( $('<option></option>').attr("value","zoomin").html("Zoom In") )
+					.append( $('<option></option>').attr("value","zoomout").html("Zoom Out") )
+					.append( $('<option></option>').attr("value","rotacion").html("Rotacion") )
+					.append( $('<option></option>').attr("value","traslacion").html("Traslacion") )
+					,
+				$('<input>').attr("name","factor").attr("placeholder","Factor")
+					.attr("value","1.5"),
+				$('<span></span>').html(" Blanco y Negro").prepend(
+					$('<input>').attr("name","byn").attr("type","checkbox"))
+			],
+			exec: function(pdi,params,trans) {
+				var factor = 1;
+				if ( params ) {
+					if ( params[0].value == "fact" && $.isNumeric(params[1].value) ) factor = params[1].value;
+					else if ( params[0].value == "umbral" && $.isNumeric(params[1].value) ) factor = function(c){
+						return ( c > params[1].value ) ? 255 : 0;
+					};
+
+					if ( params[2] && params[2].value == "on" ) {
+						pdi.require("blanco_negro");
+						pdi.dispose();
+						console.log("Blanco y negro");
+					}
+				}
+
+				if ( !trans ) trans = new Transformacion_color(factor);
+
+				pdi.loop(function(r,g,b){
+					return trans.exec(r,g,b);
+				});
+			}
+		},*/
 	}
 
 	// ************************
@@ -418,6 +584,8 @@
 			this.effects.$select.append( $('<option></option>').val("").html(" -- Elegir efecto --").attr("selected","selected") );
 			for ( var key in efectos )
 				this.effects.$select.append( $('<option></option>').val(key).html(efectos[key].nom) );
+
+			this.source.$btn.html("Cargar").attr("disabled", false);
 
 			// Eventos:
 			this.setEvents();
@@ -469,11 +637,12 @@
 				self.imagenRemoved();
 			});
 
-			ui.effects.$select.on('change',function(){
+			this.effects.$select.on('change',function(){
 				if ( self.effects.$select.val() == "" ) {
 					self.effects.$btn.attr("disabled",true);
 					return;
 				} else self.effects.$btn.attr("disabled",false);
+				
 				self.effects.$form.empty();
 				if ( efectos[self.effects.$select.val()].require == null ) return;
 				var appends = efectos[self.effects.$select.val()].require;
@@ -482,10 +651,16 @@
 				});
 			});
 
-			ui.effects.$btn.on('click',function(){
+			this.effects.$btn.on('click',function(){
+				if ( self.effects.$select.val() == "") return;
+
+				self.effects.$btn.html("Renderizando...").attr("disabled", true);
+
 				self.emptySnapshot();
-				if ( self.effects.$select.val() == "" ) return;
+
 				new PDI(self.img).run( self.effects.$select.val() , self.effects.$form.serializeArray() );
+
+				self.effects.$btn.html("Render").attr("disabled", false);
 			});
 
 		},
